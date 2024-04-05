37ee4b7fb81417a7700aeedf698d6dfffc338e8f
mirzakarabulut <mirzakara1234@gmail.com>
2024-04-05 13:26:28 +0300

    deleting useless function

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,45 +1,56 @@
 const Tour = require("./../models/tourModel");
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
+exports.getAllTour = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: "success",
+      requestdate: req.requestTime,
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
       status: "fail",
-      message: "Missing name or price",
+      message: err,
     });
   }
-  next();
-};
-exports.getAllTour = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    requestdate: req.requestTime,
-    // results: tours.length,
-    // data: {
-    //   tours,
-    // },
-  });
 };
 
-exports.getTour = (req, res) => {
-  // convert string to number
-  const id = req.params.id * 1;
-  // tour store that (id) if there is req.params === el.id
-  // const tour = tours.find((el) => el.id === id);
-  // res.status(200).json({
-  //   status: "success",
-  //   data: {
-  //     tour,
-  //   },
-  // });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: "Invalid ID number!",
+    });
+  }
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: "success",
-    data: {
-      tour: newTour,
-    },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: "Invalid data sent!",
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 // router.param("id", checkID);
 
-router.route("/").get(getAllTour).post(checkBody, createTour);
+router.route("/").get(getAllTour).post(createTour);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
