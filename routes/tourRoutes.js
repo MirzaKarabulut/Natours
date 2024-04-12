31b27cc92a1cf61c0923deb38588727f3c20ecbf
mirzakarabulut <mirzakara1234@gmail.com>
2024-04-12 15:41:14 +0300

    gitignore

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,17 +1,19 @@
 const express = require("express");
-const {
+const tourController = ({
   getAllTour,
   getTour,
   createTour,
   updateTour,
   deleteTour,
-  checkBody,
-} = require("./../controllers/tourController");
+} = require("./../controllers/tourController"));
 
 const router = express.Router();
 
 // router.param("id", checkID);
 
+router
+  .route("/top-5-cheap")
+  .get(tourController.aliasTopTours, tourController.getAllTour);
 router.route("/").get(getAllTour).post(createTour);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 
