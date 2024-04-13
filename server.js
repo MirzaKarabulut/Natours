ff18feec3dea6bdf5736364f5f9424a79d087633
mirzakarabulut <mirzakara1234@gmail.com>
2024-04-13 23:42:11 +0300

    db connections fixed

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ mongoose
     console.log("DB connections successful");
   });
 
-// console.log(process.env);
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`App running on port ${port}..`);
