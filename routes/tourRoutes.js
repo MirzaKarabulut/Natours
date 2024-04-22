f35216664e457ac04b49d5198c945031aa1f8c45
mirzakarabulut <mirzakara1234@gmail.com>
2024-04-22 12:25:52 +0300

    aggregation pipeline and virtual data

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -15,6 +15,7 @@ router
   .route("/top-5-cheap")
   .get(tourController.aliasTopTours, tourController.getAllTour);
 router.route("/tour-stats").get(tourController.getTourStats);
+router.route("/monthly-plan/:year").get(tourController.getMonthlyPlan);
 router.route("/").get(getAllTour).post(createTour);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 
