df5c98c2de50bee24ed4c4cd1b01d17d7eb5c9ba
mirzakarabulut <mirzakara1234@gmail.com>
2024-04-22 21:08:37 +0300

    document and query middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const toursSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const toursSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, "A tour must have a duration!!"],
@@ -63,6 +65,33 @@ const toursSchema = new mongoose.Schema(
 // we are adding virtual data to database
 toursSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;
+});
+
+// Document Middleware
+toursSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// toursSchema.pre("save", function (next) {
+//   console.log("Will save document..");
+//   next();
+// });
+
+// toursSchema.pre("save", function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
+
+// Query Middleware
+toursSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+toursSchema.pre(/^find/, function (doc, next) {
+  console.log(doc);
+  next();
 });
 
 const Tour = mongoose.model("Tour", toursSchema);
