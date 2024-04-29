97e42680e83addadd42f3293c0e3419d1afe135a
mirzakarabulut <mirzakara1234@gmail.com>
2024-04-29 10:59:41 +0300

    error outside express(unhandled rejection)

--- server.js
+++ server.js
@@ -23,6 +23,15 @@ mongoose
   });
 
 const port = process.env.PORT;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}..`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED REJECTİON 💣 Shutting down... ");
+  // gracefully shut down
+  server.close(() => {
+    process.exit(1);
+  });
 });
