799d7b56085840351acd1244acf5a085826f2d1d
mirzakarabulut <mirzakara1234@gmail.com>
2024-05-03 21:42:02 +0300

    chore: Refactor authController.js and add login functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,12 @@ const catchAsync = require("./../utils/catchAsync");
 const jwt = require("jsonwebtoken");
 const AppError = require("../utils/appError");
 
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -11,9 +17,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: "success",
@@ -24,7 +28,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.login = (req, res, next) => {
+exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
 
   // 1)  Check if email and password exist
@@ -33,8 +37,17 @@ exports.login = (req, res, next) => {
   }
 
   // 2) Check if user exist && password is correct
-  if (email && password) {
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user || !(await user.verifyPassword(password))) {
+    return next(new AppError("Invalid email or password!!", 401));
   }
 
   // 3) If everything is OK, send token to client
-};
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: "success",
+    token,
+  });
+});
