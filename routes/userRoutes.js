4ebf8d2cbb53a1349851e4345d35da7c5c2751a0
mirzakarabulut <mirzakara1234@gmail.com>
2024-05-24 09:49:25 +0300

    chore: Add forgot password and reset password routes in userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,5 +16,7 @@ router
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
+router.post("/forgotPassword", authController.forgotPassword);
+router.post("/resetPassword", authController.resetPassword);
 
 module.exports = router;
